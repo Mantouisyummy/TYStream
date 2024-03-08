3873f05fba3b12cb15f070870c7eec77a5671002
Mantouisyummy <51238168+Mantouisyummy@users.noreply.github.com>
2024-03-08 15:04:31 +0800

    Update(setup.py): add classifiers

--- setup.py
+++ setup.py
@@ -21,7 +21,9 @@ setup(name='tystream',
       keywords=['Twitch', 'Youtube', 'stream', 'stream Notification', 'Notification'],
       license='GNU',
       classifiers=[
-          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Operating System :: OS Independent',
           'Natural Language :: English'
